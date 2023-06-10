0b15634749de2183ce5ce37d0c6dfe1964146963
falakian <ali.falakian20@gamil.com>
2023-06-10 17:25:21 +0330

    Server codes

--- server.py
+++ server.py
@@ -1,12 +1,67 @@
 import socket
 import threading
+import struct
 def handle_user_connection(connection: socket.socket, address: tuple):
      while True:
             msg = connection.recv(2048)
             if msg :
-                 pass
+                typ,strcode,message_recv = struct.unpack('b 15s 2000s',msg)
+                message_recv=message_recv.decode('utf-8')
+                message_recv=message_recv.replace("\0", "")
+                strcode=strcode.decode('utf-8')
+                strcode=strcode.replace("\0", "")
+                if typ ==1:
+                    print("{} : It requested the list of active clients from the server".format(strcode))
+                    print(" ")
+                    packing=str(len(active_connections))+" _ "
+                    packing+=" _ ".join(active_connections.keys())
+                    packet = struct.pack('b 15s 2000s ', 10 ,bytes("Server", 'utf-8'), bytes(packing, 'utf-8'))
+                    connection.send(packet)
+                    print("{} : The server sent a list of active clients".format(strcode))
+                    print(" ")
+                elif typ == 2:
+                    if message_recv == "active":
+                        print("{} : Request to the server to activate".format(strcode))
+                        print(" ")
+                        if strcode in active_connections.keys():
+                            packet = struct.pack('b 15s 2000s', 12 ,bytes("Server", 'utf-8'),bytes("You were already in the list of active clients", 'utf-8'))
+                            connection.send(packet)
+                            print("{} : It has been active before".format(strcode))
+                            print(" ")
+                        else:
+                            active_connections[strcode]=address
+                            packet = struct.pack('b 15s 2000s', 12 ,bytes("Server", 'utf-8'),bytes("Success _ You are activated", 'utf-8'))
+                            connection.send(packet)
+                            print("{} : Activated successfully".format(strcode))
+                            print(" ")
+                    elif message_recv == "inactivate":
+                        print("{} : Request to server to inactive".format(strcode))
+                        print(" ")
+                        if strcode in active_connections.keys():
+                            del active_connections[strcode]
+                            packet = struct.pack('b 15s 2000s', 12 ,bytes("Server", 'utf-8'),bytes("Success _ You have been deactivated", 'utf-8'))
+                            connection.send(packet)
+                            print("{} : Deactivated successfully".format(strcode))
+                            print(" ")
+                        else:
+                            packet = struct.pack('b 15s 2000s', 12 ,bytes("Server", 'utf-8'),bytes("You are not active to become inactive", 'utf-8'))
+                            connection.send(packet)
+                            print("{} : It has been inactive".format(strcode))
+                            print(" ")
+                    else :
+                        print("{} : This request is unknown to the server".format(strcode))
+                        print(" ")
+                        packet = struct.pack('b 15s 2000s', 12 ,bytes("Server", 'utf-8'),bytes("The command was wrong", 'utf-8'))
+                        connection.send(packet)
+                elif typ == 3:
+                    pass
+                elif typ == 4:
+                     pass
+                else:
+                     pass
+            
             else:
-                connection.close()
+                    connection.close()
   
 listening_port = 12000
 connections = {}
